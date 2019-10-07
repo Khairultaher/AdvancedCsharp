c37a12ded7c153065bdb332f6252a37c11c85e63
Khairul Alam <khairul.alam@datasoft-bd.com>
2019-10-07 11:01:03 +0600

    Shallow Copy deep copy added

---
+++ EventDeleget/Video.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventDeleget
+{
+    public class Video
+    {
+        public string Title { get; set; }
+    }
+}
