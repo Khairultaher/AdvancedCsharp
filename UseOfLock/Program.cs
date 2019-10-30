7c53df73c4d45613c6dc64a51951402194b77f02
Khairul Alam <khairul.alam@datasoft-bd.com>
2019-10-30 14:27:56 +0600

    Lock

---
+++ UseOfLock/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+namespace UseOfLock
+{
+    class Program
+    {
+        //static Maths maths = new Maths();
+        static void Main(string[] args)
+        {
+            Maths maths = new Maths();
+            Thread thread1 = new Thread(maths.Devide); // Child (1) thread within main thread
+            thread1.Start();
+
+            maths.Devide();
+
+            Thread thread2 = new Thread(maths.Devide); // Child (2) thread within main thread
+            thread1.Start();
+
+            Console.ReadLine();
+        }
+    }
+
+    class Maths
+    {
+        public int num1;
+        public int num2;
+        Random ran = new Random();
+        public void Devide()
+        {
+            lock (this)
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    try
+                    {
+                        num1 = ran.Next(1, 2);
+                        num2 = ran.Next(1, 2);
+
+                        int result = num1 / num2;
+
+                        num1 = 0;
+                        num2 = 0;
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+                }
+            }
+        }
+    }
+}
