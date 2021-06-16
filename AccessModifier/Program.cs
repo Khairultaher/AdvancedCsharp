f5c5a62d8d0ea0736ec48be36735c80bcac52f3b
Khairul Taher <khairultaher@outlook.com>
2021-06-16 10:44:03 +0600

    2021-06-17.k3

--- AccessModifier/Program.cs
+++ AccessModifier/Program.cs
@@ -2,6 +2,15 @@
 
 namespace AccessModifier
 {
+    // The default access for everything in C# is "the most restricted access you could declare for that member".
+    // top level class: internal
+    // method: private
+    // members(unless an interface or enum): private (including nested classes)
+    // members(of interface or enum): public
+    // constructor: private (note that if no constructor is explicitly defined, a public default constructor will be automatically defined)
+    // delegate: internal
+    // interface: internal
+    // explicitly implemented interface member : public!
     class Program
     {
         static void Main(string[] args)
