db8aa083ef5cea9f36568436b6a31cd2cbe4c606
Khairul Taher <khairultaher@outlook.com>
2021-06-20 14:56:53 +0600

    2021-06-20.3k

--- CsharpNine/Program.cs
+++ CsharpNine/Program.cs
@@ -4,9 +4,26 @@ namespace CsharpNine
 {
     class Program
     {
+        //public record Person(string FirstName, string LastName);
+        public record Person(string FirstName, string LastName, string[] PhoneNumbers);
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Person person = new("Nancy", "Davolio");
+            var phoneNumbers = new string[2];
+            Person person1 = new("Nancy", "Davolio", phoneNumbers);
+            Person person2 = new("Nancy", "Davolio", phoneNumbers);
+            Console.WriteLine(person1 == person2); // output: True
+
+            person1.PhoneNumbers[0] = "555-1234";
+            Console.WriteLine(person1 == person2); // output: True
+
+            Console.WriteLine(ReferenceEquals(person1, person2)); // output: False
+
+
+            Console.ReadLine();
         }
+
+
+
     }
 }
