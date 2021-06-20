2d9dcaf87f46aa0ea14d9fcf45ca3e7c7ac8f659
Khairul Taher <khairultaher@outlook.com>
2021-06-20 12:50:45 +0600

    2021-06-20.1k

---
+++ CsharpTen/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CsharpTen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }   
+        
+    }
+}
