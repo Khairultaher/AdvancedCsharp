7108b6277dea1f4b8844d9d9e9a1b3be324bcf36
khairul alam <khairul.alam@vivacomsolutions.com>
2023-01-26 18:03:56 +0600

    updated the dp description

--- ParallelForForEach/Program.cs
+++ ParallelForForEach/Program.cs
@@ -1,8 +1,5 @@
-﻿
-//CallParallelFor();
-//CallParallelForEach();
+﻿using Polly;
 
-await CallParallelForEachWithOption();
 
 void CallParallelFor()
 {
@@ -10,7 +7,6 @@ void CallParallelFor()
         Console.WriteLine("Thread {0} processing value {1}", Thread.CurrentThread.ManagedThreadId, i);
     });
 }
-
 
 void CallParallelForEach()
 {
@@ -26,10 +22,35 @@ async Task CallParallelForEachWithOption()
     List<string> values = new List<string> { "value1", "value2", "value3", "value4", "value5", "value6", "value7" };
     ParallelOptions options = new ParallelOptions { MaxDegreeOfParallelism = 5 };
     await Parallel.ForEachAsync(values, options, async (value, cancellationToken) => {
-        await Task.Delay(1000);
+        await Task.Delay(3000);
         Console.WriteLine("Thread {0} processing value {1}", Thread.CurrentThread.ManagedThreadId, value);
     });
 }
+
+
+await ParallelForEachWithRetryPolicy();
+async Task ParallelForEachWithRetryPolicy()
+{
+    List<string> values = new List<string> { "value1", "value2", "value3", "value4", "value5", "value6", "value7" };
+    var retryPolicy = Policy
+        .Handle<Exception>()
+        .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(retryAttempt));
+
+    ParallelOptions options = new ParallelOptions { MaxDegreeOfParallelism = 3 };
+    await Parallel.ForEachAsync(values, options, async (value, CancellationToken) => {
+        await retryPolicy.ExecuteAsync(async () => {
+            await DoSomethingAsync(value);
+        });
+    });
+
+}
+
+static async Task DoSomethingAsync(string value)
+{
+    await Task.Delay(1000);
+    Console.WriteLine("Thread {0} processing value {1}", Thread.CurrentThread.ManagedThreadId, value);
+}
+
 
 
 
