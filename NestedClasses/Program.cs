7378814f9da3f5e11e2a934565ea917c16851bc1
khairul alam <khairul.alam@vivacomsolutions.com>
2023-01-26 20:20:13 +0600

    updated the dp description

---
+++ NestedClasses/Program.cs
@@ -0,0 +1,71 @@
+﻿
+Car myCar = new Car("Toyota", "Corolla", 2020);
+myCar.EngineDetails.Horsepower = 140;
+myCar.EngineDetails.Cylinders = 4;
+
+Console.WriteLine("Make: " + myCar.Make); // "Toyota"
+Console.WriteLine("Model: " + myCar.Model); // "Corolla"
+Console.WriteLine("Year: " + myCar.Year); // 2020
+Console.WriteLine("Horsepower: " + myCar.EngineDetails.Horsepower); // 140
+Console.WriteLine("Cylinders: " + myCar.EngineDetails.Cylinders); // 4
+
+
+public class Car
+{
+    private string make;
+    private string model;
+    private int year;
+    private Engine engine;
+
+    public Car(string make, string model, int year)
+    {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.engine = new Engine();
+    }
+
+    public class Engine
+    {
+        private int horsepower;
+        private int cylinders;
+
+        public Engine()
+        {
+            horsepower = 0;
+            cylinders = 0;
+        }
+
+        public int Horsepower
+        {
+            get { return horsepower; }
+            set { horsepower = value; }
+        }
+
+        public int Cylinders
+        {
+            get { return cylinders; }
+            set { cylinders = value; }
+        }
+    }
+
+    public string Make
+    {
+        get { return make; }
+    }
+
+    public string Model
+    {
+        get { return model; }
+    }
+
+    public int Year
+    {
+        get { return year; }
+    }
+
+    public Engine EngineDetails
+    {
+        get { return engine; }
+    }
+}
---
+++ PartialClasses/Program.cs
@@ -0,0 +1,57 @@
+﻿
+
+// File: Car.cs
+public partial class Car
+{
+    private string make;
+    private string model;
+    private int year;
+
+    public Car(string make, string model, int year)
+    {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public string Make
+    {
+        get { return make; }
+    }
+
+    public string Model
+    {
+        get { return model; }
+    }
+
+    public int Year
+    {
+        get { return year; }
+    }
+}
+
+// File: CarDetails.cs
+public partial class Car
+{
+    private int horsepower;
+    private int cylinders;
+
+    public Car()
+    {
+        horsepower = 0;
+        cylinders = 0;
+    }
+
+    public int Horsepower
+    {
+        get { return horsepower; }
+        set { horsepower = value; }
+    }
+
+    public int Cylinders
+    {
+        get { return cylinders; }
+        set { cylinders = value; }
+    }
+}
+
