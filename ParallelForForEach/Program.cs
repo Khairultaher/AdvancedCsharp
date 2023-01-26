0823439865b3f66b6cbf485bf6873a23ed781a91
khairul alam <khairul.alam@vivacomsolutions.com>
2023-01-26 17:26:00 +0600

    updated the dp description

---
+++ ParallelForForEach/Program.cs
@@ -0,0 +1,36 @@
+﻿
+//CallParallelFor();
+//CallParallelForEach();
+
+await CallParallelForEachWithOption();
+
+void CallParallelFor()
+{
+    Parallel.For(0, 10, i => {
+        Console.WriteLine("Thread {0} processing value {1}", Thread.CurrentThread.ManagedThreadId, i);
+    });
+}
+
+
+void CallParallelForEach()
+{
+    List<string> values = new List<string> { "value1", "value2", "value3" };
+    Parallel.ForEach(values, value => {
+        Console.WriteLine("Thread {0} processing value {1}", Thread.CurrentThread.ManagedThreadId, value);
+    });
+
+}
+
+async Task CallParallelForEachWithOption()
+{
+    List<string> values = new List<string> { "value1", "value2", "value3", "value4", "value5", "value6", "value7" };
+    ParallelOptions options = new ParallelOptions { MaxDegreeOfParallelism = 5 };
+    await Parallel.ForEachAsync(values, options, async (value, cancellationToken) => {
+        await Task.Delay(1000);
+        Console.WriteLine("Thread {0} processing value {1}", Thread.CurrentThread.ManagedThreadId, value);
+    });
+}
+
+
+
+
