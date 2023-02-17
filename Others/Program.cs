bd8242eac101293f02cbf8ebd3a3f4d3bfb043d3
khairul alam <khairul.alam@vivacomsolutions.com>
2023-02-17 14:15:02 +0600

    updated

---
+++ Others/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("------------i++-------------");
+int res = 0;
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(res++);
+}
+
+Console.WriteLine("----------++i---------------");
+res = 0;
+for (int i = 0; i < 5; ++i)
+{
+    Console.WriteLine(++res);
+}
+Console.ReadLine();
---
+++ xUnitTestProject/UnitTest.cs
@@ -0,0 +1,14 @@
+namespace xUnitTestProject
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(1);
+            }
+        }
+    }
+}
