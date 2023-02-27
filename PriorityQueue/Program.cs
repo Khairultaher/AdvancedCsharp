cfd9a6a8d4f3d4eb6d9a1ed9567efd9e10e4606f
khairul alam <khairul.alam@vivacomsolutions.com>
2023-02-27 13:24:22 +0600

    updated

---
+++ PriorityQueue/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Create a priority queue of integers
+PriorityQueue<int,int> queue = new PriorityQueue<int,int>();
+
+// Add some items to the queue
+
+queue.Enqueue(5, queue.Count + 1); // 1
+queue.Enqueue(3, queue.Count + 1);
+queue.Enqueue(7, queue.Count + 1);
+
+// Get the item with the highest priority (which is 7)
+int highestPriorityItem = queue.Peek();
+
+// Remove the item with the highest priority (which is 7)
+int dequeuedItem = queue.Dequeue();
+
+
+
+Console.ReadLine();
