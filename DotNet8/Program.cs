c2c9f73d93205959e0fbffc69e3c4361c7d20bc9
khairul alam <khairul.alam@vivacomsolutions.com>
2023-07-06 14:31:07 +0600

    .net8 added

---
+++ DotNet8/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+
+
+#region HTTP/3
+
+using var client = new HttpClient
+{
+    DefaultRequestVersion = HttpVersion.Version30,
+    DefaultVersionPolicy = HttpVersionPolicy.RequestVersionExact
+};
+
+Console.WriteLine("--- localhost:5001 ---");
+
+HttpResponseMessage resp = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/1");
+string body = await resp.Content.ReadAsStringAsync();
+
+Console.WriteLine(
+    $"status: {resp.StatusCode}, version: {resp.Version}, " +
+    $"body: {body.Substring(0, Math.Min(100, body.Length))}");
+
+#endregion
